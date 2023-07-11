6cf9a177d645dce980e13bcaa2f4ae3a8cdd1c2b
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-11 17:44:19 -0400

    Custom hook: add hook of notes, contains useEffect of all notes

---
+++ app/src/hooks/useNotes.js
@@ -0,0 +1,20 @@
+import { useState,useEffect } from "react"
+import { getAllNotes } from "../services/notes.js"
+
+export const useNotes = () =>{
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+		getAllNotes()
+			.then(nota => {
+				setNotes(nota)
+			}).catch((error) => {
+				console.error('database error: ', error)
+			})
+	}, [])
+    return {
+        notes,
+		setNotes
+    }
+
+}
