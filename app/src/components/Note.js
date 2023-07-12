a29ef0f09c306a5c3f603c0312f2d9f7b75924f8
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-11 20:28:56 -0400

    improvement: change li for div

--- app/src/components/Note.js
+++ app/src/components/Note.js
@@ -2,8 +2,8 @@ import {Link} from 'react-router-dom'
 
 export const Note = ({ note }) => {
 	return (
-		<li>
+		<div>
 			<Link to={`/notes/${note.id}`}><p>{note.content}</p></Link>
-		</li>
+		</div>
 	)
 }
