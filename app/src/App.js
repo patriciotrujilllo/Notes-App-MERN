3660698d4b989b592a4892f1834bc6b27d405599
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-07-11 20:30:42 -0400

    improvement: react-bootstrap components are used, Navbar,Brand,Toogle,Collapse

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,20 @@
-import {React} from "react"
+import {React,Suspense} from "react"
 import {Link,BrowserRouter,Route,Routes,Navigate} from 'react-router-dom'
 import Notes from './Notes.js'
 import Login from "./login.js"
 import { SingleNote } from "./components/singleNote.js"
 import { useUser } from "./hooks/useUser.js"
 import { useNotes } from "./hooks/useNotes.js"
+import { Container, Nav, Navbar } from "react-bootstrap"
 
-const Home = () => <h1>Home</h1>
+const Home = () => {
+	return(
+		<div>
+			<title>Home | Notes app</title>
+			<h1>Home page</h1>
+		</div>
+	)
+}
 const Users = () => <h1>Users</h1>
 
 function App () {
@@ -15,38 +23,63 @@ function App () {
 	const {notes} = useNotes()
 
 	return (
-		<BrowserRouter>
-			<header>
-				<Link to="/" style={{ padding: "5px" }}>
-					Home
-				</Link>
-				<Link to="/notes" style={{ padding: "5px" }}>
-					Notes
-				</Link>
-				<Link to="/users" style={{ padding: "5px" }}>
-					Users
-				</Link>
-				{user ? 
-					<em>{user.username}</em>
-					:
-					<Link to="/login" style={{ padding: "5px" }}>
-					Login
-					</Link>
-				}
+		<Suspense fallback={<div>Loading...</div>}>
+		<div className="container">
+			<BrowserRouter>
+				<Navbar expand="lg" className="bg-body-tertiary">
+					<Container>
+					<Navbar.Brand href='/'>App-Notes</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+					<Navbar.Collapse id="id-collapse-navbar">
+						<Nav className="me-auto">
+
+							<Nav.Link>
+								<Link to="/notes" style={{ padding: "5px" }}>
+									Notes
+								</Link>
+							</Nav.Link>
+
+							<Nav.Link>
+								<Link to="/users" style={{ padding: "5px" }}>
+									Users
+								</Link>
+							</Nav.Link>
+
+							<Nav.Link>
+							{user ? 
+								<em>{user.username}</em>
+								:
+								<Link to="/login" style={{ padding: "5px" }}>
+								Login
+								</Link>
+							}
+							</Nav.Link>
+							
+							
+							
+						</Nav>
+						
+					</Navbar.Collapse>
+					
+					</Container>
+					
+					
+				</Navbar>
+				<Routes>
+					<Route path="/notes/:id" element={<SingleNote notes={notes}/>}/>
+					<Route path="/notes" element={<Notes/>}/>
+					<Route path="/users" element={<Users/>}/>
+					<Route path="/login" 
+					element={user? 
+					<Navigate to='/notes'/>
+					:<Login/>} 
+					/>
+					<Route path="/" element={<Home/>}/>
+				</Routes>
 				
-			</header>
-			<Routes>
-				<Route path="/notes/:id" element={<SingleNote notes={notes}/>}/>
-				<Route path="/notes" element={<Notes/>}/>
-				<Route path="/users" element={<Users/>}/>
-				<Route path="/login" 
-				element={user? <Navigate to='/notes'/>:<Login/>} 
-				/>
-				<Route path="/notes" element={<Login/>}/>
-				<Route path="/" element={<Home/>}/>
-			</Routes>
-			
-		</BrowserRouter>
+			</BrowserRouter>
+		</div>
+		</Suspense>
 	)
 	}
 export default App
