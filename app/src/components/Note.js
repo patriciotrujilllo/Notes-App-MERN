b38570342041de2ffe6eeb24f9519a1ca9628b55
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-01 20:05:35 -0400

    Update note: cada nota tendra dos iconos, el edit y el delete, solo en caso de que exista un usuario logeado

--- app/src/components/Note.js
+++ app/src/components/Note.js
@@ -1,9 +1,22 @@
 import {Link} from 'react-router-dom'
+import {AiFillEdit,AiTwotoneDelete} from 'react-icons/ai'
+import { useUser } from '../hooks/useUser'
 
 export const Note = ({ note }) => {
+	const {user} = useUser()
 	return (
-		<div>
-			<Link to={`/notes/${note.id}`}><p>{note.content}</p></Link>
+		<div className='note'>
+			<Link className='note-link' to={`/notes/${note.id}`}><p>{note.content}</p></Link>
+			<div className='icons-note'>
+			{user ? (
+				<>
+				<AiFillEdit size='1.5rem'/>
+				<AiTwotoneDelete size='1.5rem'/>
+				</>
+			)
+			: ''}
+			</div>
+			
 		</div>
 	)
 }
