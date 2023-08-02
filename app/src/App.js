035fc6567a84ca52e8a3fb7dd808d3ddf612b4e3
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-01 20:04:32 -0400

    Move navbar: se movio el navbar en un componente aparte

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
 import {React,Suspense} from "react"
-import {Link,BrowserRouter,Route,Routes,Navigate} from 'react-router-dom'
+import {BrowserRouter,Route,Routes,Navigate} from 'react-router-dom'
 import Notes from './Notes.js'
 import Login from "./login.js"
 import { SingleNote } from "./components/singleNote.js"
 import { useUser } from "./hooks/useUser.js"
 import { useNotes } from "./hooks/useNotes.js"
-import { Container, Nav, Navbar } from "react-bootstrap"
+import {NavHeader} from './components/NavHeader.js'
+import './Styles.css'
 
 const Home = () => {
 	return(
@@ -15,7 +16,6 @@ const Home = () => {
 		</div>
 	)
 }
-const Users = () => <h1>Users</h1>
 
 function App () {
 
@@ -26,49 +26,10 @@ function App () {
 		<Suspense fallback={<div>Loading...</div>}>
 		<div className="container">
 			<BrowserRouter>
-				<Navbar expand="lg" className="bg-body-tertiary">
-					<Container>
-					<Navbar.Brand href='/'>App-Notes</Navbar.Brand>
-					<Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-					<Navbar.Collapse id="id-collapse-navbar">
-						<Nav className="me-auto">
-
-							<Nav.Link>
-								<Link to="/notes" style={{ padding: "5px" }}>
-									Notes
-								</Link>
-							</Nav.Link>
-
-							<Nav.Link>
-								<Link to="/users" style={{ padding: "5px" }}>
-									Users
-								</Link>
-							</Nav.Link>
-
-							<Nav.Link>
-							{user ? 
-								<em>{user.username}</em>
-								:
-								<Link to="/login" style={{ padding: "5px" }}>
-								Login
-								</Link>
-							}
-							</Nav.Link>
-							
-							
-							
-						</Nav>
-						
-					</Navbar.Collapse>
-					
-					</Container>
-					
-					
-				</Navbar>
+				<NavHeader/>
 				<Routes>
 					<Route path="/notes/:id" element={<SingleNote notes={notes}/>}/>
 					<Route path="/notes" element={<Notes/>}/>
-					<Route path="/users" element={<Users/>}/>
 					<Route path="/login" 
 					element={user? 
 					<Navigate to='/notes'/>
